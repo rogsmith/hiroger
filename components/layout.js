15706e8fe367de1a2aaf486353f4bff3600bc094
Roger Smith <roger.smith@realtyclub.com>
2021-02-18 11:30:06 -0800

    let roger help you

--- components/layout.js
+++ components/layout.js
@@ -21,6 +21,7 @@ export default function Layout({ children, home }) {
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
+        <title>Meet Roger | Medicare is complicated, he makes it simple.</title>
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
